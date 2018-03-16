0cd689c1e16e89ac009ba6b21e335fce1026e75e
JR <john@addlocal.com>
2018-03-16 16:44:24 -0400

    [remove submodule] delete themes/hugo-theme-bootstrap4-blog/.git

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # deploy-hugo-netlifycms
+
+
+## How to Install
+
+```
+git clone git@github.com:johnrlive/deploy-hugo-netlifycms.git
+cd deploy-hugo-netlifycms/
+bash hugo-cms.sh
+```
+
+
+Replace this line in the this file site/config.toml
+
+```
+- baseurl = "https://example.org/"
++ baseurl = "/"
+```
--- hugo-cms.sh
+++ hugo-cms.sh
@@ -22,7 +22,9 @@ echo "=== git clone theme"
 sleep 1
 cd site/themes/
 git clone https://github.com/alanorth/hugo-theme-bootstrap4-blog
+rm -rf hugo-theme-bootstrap4-blog/.git/
 cd .. # site folder
+
 rm config.toml
 cp -vfR themes/hugo-theme-bootstrap4-blog/exampleSite/content/post/ ./content/post/
 cp -vfR themes/hugo-theme-bootstrap4-blog/exampleSite/config.toml ./
