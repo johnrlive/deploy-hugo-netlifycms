3316c256984f36108b53533f21d403c828e6940e
JR <john@addlocal.com>
2018-03-17 13:22:36 -0400

    split cms and theme scripts

--- hugo-cms.sh
+++ hugo-cms.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-rm -rf one-click-hugo-cms/
+sitename="johnrlive"
 
-echo "=== download one-click-hugo-cms repo"
-git clone https://github.com/netlify-templates/one-click-hugo-cms
-cd one-click-hugo-cms
+## uncomment to delte site name
+# rm -rf $sitename
+
+echo "=== download one-click-hugo-cms repo as $sitename ==="
+git clone https://github.com/netlify-templates/one-click-hugo-cms $sitename
+cd $sitename
 rm -rf .git/
+npm install
 
-### Local copy of one-click-hugo-cms
+## Local copy of one-click-hugo-cms
+# rm -rf one-click-hugo-cms
 # cp test/one-click-hugo-cms/. ./
 # cd one-click-hugo-cms
 
-echo "=== delete layouts file"
-rm -rf site/layouts/
-rm -rf site/content/*
+## Run the theme script
+bash hugo-theme.sh
 
-echo "=== mkdir site/themes/"
-sleep 1
-mkdir site/themes/
-
-echo "=== git clone theme"
-sleep 1
-cd site/themes/
-git clone https://github.com/alanorth/hugo-theme-bootstrap4-blog
-rm -rf hugo-theme-bootstrap4-blog/.git/
-cd .. # site folder
-
-rm config.toml
-cp -vfR themes/hugo-theme-bootstrap4-blog/exampleSite/content/post/ ./content/post/
-cp -vfR themes/hugo-theme-bootstrap4-blog/exampleSite/config.toml ./
-
-# start Server
-npm install
+## Start Server
 npm start
